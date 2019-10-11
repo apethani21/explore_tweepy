e9ab577dba6e90d916c93c0911407a3e93312fc1
Ajay Pethani <ajaypethani97@gmail.com>
2019-10-12 00:01:15 +0100

    html for neater formatting

--- email_me_tweets.py
+++ email_me_tweets.py
@@ -6,6 +6,22 @@ import sys
 import os
 import tweepy
 import tweepy_utils
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+def create_tweet_html_body(time, tweet):
+    body = f"""\
+    <html>
+      <body>
+        <p>
+        <b> {time} </b><br>
+        {tweet} <br>
+        </p>
+      </body>
+    </html>
+    """
+    return body
 
 
 def get_email_credentials():
@@ -17,15 +33,18 @@ def get_email_credentials():
 
 def send_myself_message(message):
     email_credentials = get_email_credentials()
-    receiver_email_address = email_credentials["receiver_email"]
-    sender_email_address = email_credentials["sender_email"]
+    receiver_email = email_credentials["receiver_email"]
+    sender_email = email_credentials["sender_email"]
+    if type(message) is MIMEMultipart:
+        message["From"] = sender_email
+        message["To"] = receiver_email
     password = email_credentials["sender_password"]
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com",
                           port=465,
                           context=context) as server:
-        server.login(sender_email_address, password)
-        server.sendmail(sender_email_address, receiver_email_address, message)
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
     return
 
 
@@ -33,9 +52,18 @@ def email_me_latest_tweets(handle='northernline', n=3):
     tweepy_auth = tweepy_utils.set_tweepy_account()
     api = tweepy.API(tweepy_auth)
     tweets = tweepy_utils.get_tweets(handle, n, api)
-    printable = set(string.printable)
-    clean_tweets = ''.join(filter(lambda x: x in printable, str(tweets)))
-    send_myself_message(f"\n{clean_tweets}")
+    message = MIMEMultipart("alternative")
+    message["Subject"] = f"{handle} latest tweets"
+    text = f"\n{tweets}"
+    html = "".join([create_tweet_html_body(time, tweet)
+                    for time, tweet in tweets.items()])
+    plain_backup = MIMEText(text, "plain")
+    html_main = MIMEText(html, "html")
+    message.attach(plain_backup)
+    message.attach(html_main)
+    #printable = set(string.printable)
+    #clean_tweets = ''.join(filter(lambda x: x in printable, str(tweets)))
+    send_myself_message(message.as_string())
     return
 
 
@@ -43,5 +71,5 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     args = dict([arg.split('=') for arg in args])
     handle = args.get('handle', 'northernline')
-    number_of_tweets = int(args.get('n', 3))
+    number_of_tweets = int(args.get('n', 2))
     email_me_latest_tweets(handle, number_of_tweets)
