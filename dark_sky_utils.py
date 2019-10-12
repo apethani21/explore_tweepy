e8e9c0489b888ed7cef0e0ab7ab33589b22783c6
Ajay Pethani <ajaypethani97@gmail.com>
2019-10-12 16:11:18 +0100

    emailing weather details and formatting

--- dark_sky_utils.py
+++ dark_sky_utils.py
@@ -1,6 +1,10 @@
 import os
 import time
 import requests
+from datetime import datetime, timedelta
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email_me_tweets import send_myself_message
 
 
 location = {
@@ -33,7 +37,9 @@ def get_weather(latitude=51.583017, longitude=-0.226472):
 def get_today_hour_minute(hour, minute):
     now = int(time.time())
     midnight = now - (now % 86400)
-    return midnight + hour*3600 + minute*60
+    midnight = datetime.utcfromtimestamp(now - (now % 86400))
+    morning = midnight + timedelta(hours=hour, minutes=minute)
+    return int(morning.strftime("%s"))
 
 
 def get_weather_hour_minute(area, hour, minute=None):
@@ -53,5 +59,40 @@ def get_weather_hour_minute(area, hour, minute=None):
     return {"area": area, **minute_datapoint, **overview}
 
 
+def create_weather_html_body(weather_info):
+    area = weather_info["area"].split('_')
+    area_clean = ' '.join([word.capitalize() for word in area])
+    ts = weather_info["time"]
+    time_clean = datetime.fromtimestamp(ts).strftime("%d %b %H:%M:%S")
+    body = f"""\
+    <html>
+      <body>
+        <p>
+        <b>Time</b>: {time_clean} -- <b>{area_clean}</b> <br>
+        <b>Summary</b>: {weather_info["summary"]} <br>
+        <b>Chance of {weather_info["precipType"]}</b>: {weather_info["precipProbability"]} <br>
+        <b>Intensity of {weather_info["precipType"]}</b>: {weather_info["precipIntensity"]} inch/hour <br>
+        </p>
+      </body>
+    </html>
+    """
+    return body
+
+
+def email_me_latest_weather(weather_updates):
+    message = MIMEMultipart("alternative")
+    message["Subject"] = f"Weather update"
+    text = '\n'.join([str(update) for update in weather_updates])
+    html = "".join([create_weather_html_body(update) for update in weather_updates])
+    plain_backup = MIMEText(text, "plain")
+    html_main = MIMEText(html, "html")
+    message.attach(plain_backup)
+    message.attach(html_main)
+    send_myself_message(message.as_string())
+    return
+
+
 if __name__ == "__main__":
-    pass
+    hendon_weather_update = get_weather_hour_minute("hendon_central", 16, 15)
+    fitzrovia_weather_update = get_weather_hour_minute("goodge_street", 16, 30)
+    email_me_latest_weather([hendon_weather_update, fitzrovia_weather_update])
